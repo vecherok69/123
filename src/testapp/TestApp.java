82df26d8d90d01c4556b0a370ef7fb3114ff08ff
Vecherok <vecherok69@gmail.com>
2018-03-22 13:57:25 +0200

    Commit 2

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -1,4 +1,5 @@
 package testapp;
+import java.util.Scanner;
 public class TestApp {
 /**
 * @param args the command line arguments
@@ -6,10 +7,12 @@ public class TestApp {
 
 public static void main(String[] args) {
 // TODO code application logic here
-float a=10;
-float b=(float) 12.5;
+Scanner in = new Scanner(System.in);
+System.out.print("1st num: ");
+double a = Double.valueOf(in.nextLine());
+System.out.print("2nd num: ");
+double b = Double.valueOf(in.nextLine());
 System.out.println("a+b="+(a+b));
 System.out.println("GIT test application");
-
 }
 }
