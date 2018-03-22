9bff9e37369c32a08162b367a4a15303af34b768
Vecherok <vecherok69@gmail.com>
2018-03-22 13:53:36 +0200

    Commit n

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package testapp;
-
-/**
- *
- * @author veche
- */
 public class TestApp {
+/**
+* @param args the command line arguments
+*/
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public static void main(String[] args) {
+// TODO code application logic here
+float a=10;
+float b=(float) 12.5;
+System.out.println("a+b="+(a+b));
+System.out.println("GIT test application");
+
+}
 }
