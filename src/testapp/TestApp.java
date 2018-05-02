7085510ccc491fe01096a2d94f5fe4ba1fbc011b
Vecherok <vecherok69@gmail.com>
2018-05-02 12:11:20 +0300

    89

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -1,18 +1,16 @@
 package testapp;
-import java.util.Scanner;
-public class TestApp {
-/**
-* @param args the command line arguments
-*/
+public class SimpleCalc {
 
-public static void main(String[] args) {
-// TODO code application logic here
-Scanner in = new Scanner(System.in);
-System.out.print("1st num: ");
-double a = Double.valueOf(in.nextLine());
-System.out.print("2nd num: ");
-double b = Double.valueOf(in.nextLine());
-System.out.println("a+b="+(a+b));
-System.out.println("GIT test application");
+private double a;
+private double b;
+
+SimpleCalc(double p1,double p2) {
+this.a=p1;
+this.b=p2;
 }
+
+public double getSum() {
+return this.a+this.b;
+}
+
 }
